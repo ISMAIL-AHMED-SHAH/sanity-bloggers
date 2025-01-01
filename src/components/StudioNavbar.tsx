dd3740c45e2c8b434a0aabea8bc020a7ec95ddb4
Ismail Ahmed <aismail@kictl.com>
2025-01-01 06:43:32 +0500

    code modified

--- src/components/StudioNavbar.tsx
+++ src/components/StudioNavbar.tsx
@@ -5,20 +5,22 @@ import Logo from "./Logo";
 const StudioNavbar = (props: any) => {
   return (
     <div>
-    <div className="p-5 bg-black text-gray-100 flex items-center justify-between">
-      <Link
-        href={"/"}
-        className="flex items-center gap-3 font-semibold hover:text-blue-600 duration-200"
-      >
-        <IoReturnDownBack className="text-2xl" />
-        Go to Website
-      </Link>
-      <Logo
-        title="Bloggers Studio"
-        className="text-2xl hidden md:inline-flex"
-      />
-      <p className="text-sm">Studio for Blog Content</p>
-    </div>
+      <div className="p-5 bg-black text-gray-100 flex items-center justify-between gap-4">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 font-semibold hover:text-blue-600 duration-200"
+        >
+          <IoReturnDownBack className="text-2xl" />
+          Go to Website
+        </Link>
+        <div className="flex items-center gap-4">
+          <Logo
+            title="Orbit'Blog Studio"
+            className="text-2xl md:inline-flex"
+          />
+          <p className="text-sm">Studio for Blog Content</p>
+        </div>
+      </div>
       {props.renderDefault(props)}
     </div>
   );
