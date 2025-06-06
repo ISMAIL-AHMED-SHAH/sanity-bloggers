2143e6001b17f52d223569fa272e7a7f53eb32f1
Ismail Ahmed <aismail@kictl.com>
2025-06-06 15:01:08 +0500

    Name change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         className="w-full max-h-screen object-contain"
       />
       <div className="absolute top-0 w-full h-full bg-black/30 text-gray-100 flex flex-col items-center justify-center">
-        <h2 className="text-7xl lg:text-[150px] font-bold">Syed Shah</h2>
+        <h2 className="text-7xl lg:text-[150px] font-bold">Ismail Ahmed Shah</h2>
         <p className="text-xl md:text-2xl lg:text-5xl font-semibold">
           A Solo Traveler,
         </p>
