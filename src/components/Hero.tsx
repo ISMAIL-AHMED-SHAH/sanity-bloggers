f196fa398650ea2ac12bc982e7a4e3b8733da94a
Ismail Ahmed <aismail@kictl.com>
2025-06-06 16:40:17 +0500

    added properties

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,10 +9,19 @@ const Hero = () => {
         alt="banner image"
         className="w-full max-h-screen object-contain"
       />
-      <div className="absolute top-0 w-full h-full bg-black/30 text-gray-100 flex flex-col items-center justify-center">
-        <h2 className="text-7xl lg:text-[150px] font-bold">Ismail Ahmed Shah</h2>
+      <div className="absolute top-0 w-full h-full bg-black/30 text-white flex flex-col items-center justify-center">
+        <h2 className="text-5xl lg:text-[120px] font-bold">Ismail Ahmed Shah</h2>
         <p className="text-xl md:text-2xl lg:text-5xl font-semibold">
-          A Solo Traveler,
+          A Solo Traveler, 
+        </p>
+        <p className="text-xl md:text-2xl lg:text-5xl font-semibold mt-11 text-yellow-200">
+          A Thinker ⟶ Builder ⟶ Explorer,
+        </p>
+        <p className="text-xl md:text-2xl lg:text-5xl font-semibold text-yellow-500">
+           Agentic AI Developer  •  Polyglot Coder
+        </p>
+        <p className="text-xl md:text-2xl lg:text-5xl font-semibold text-yellow-700">
+           Turning Algorithms into Action 
         </p>
       </div>
     </div>
